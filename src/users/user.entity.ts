928df25e8262beb02642754114b2bbb8d1d1e464
Viviane Rocha <202301022151@alunos.estacio.br>
2023-04-01 12:16:32 -0300

    inclusão do repository e metodos crete e findone

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -6,11 +6,23 @@ export class User {
   id: number;
 
   @Column()
-  username: string;
-
-  @Column()
   email: string;
   
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert(){
+    console.log('Inserted User with id', this.id)
+  }
+
+  @AfterUpdate()
+  logUpdate(){
+    console.log('Updated user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove(){
+    console.log('Removed user with id', this.id);
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,24 @@
-import {Body, Controller, Post, Get } from '@nestjs/common';
+import {Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService){}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto){
+    this.userService.create(body.email, body.password);
     console.log(body);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string){
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  listAll(@Query('email') email: string){
+    return this.userService.find(email);
   }
 }
